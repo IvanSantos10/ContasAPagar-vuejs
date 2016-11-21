1bdb6b671b6800efd90fe93cd2c6285b8bdf9fdd
Ivan Santos <ivannildosanto@hotmail.com>
2016-11-21 16:36:53 -0300

    implementando Materialize CSS

--- src/bill-pay/bill-pay-list.component.js
+++ src/bill-pay/bill-pay-list.component.js
@@ -11,33 +11,37 @@ window.billPayListComponent = Vue.extend({
             background-color: burlywood;
         }
     </style>
-    <table border="1" cellpadding="10">
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Vencimento</th>
-            <th>Nome</th>
-            <th>Valor</th>
-            <th>Paga?</th>
-            <th>Ações</th>
-        </tr>
-        </thead>
-        <tdody>
-            <tr v-for="(index,o) in bills">
-                <td>{{ index + 1}}</td>
-                <td>{{ o.date_due | dateFormat 'pt-BR'}}</td>
-                <td>{{ o.name | stringFormat }}</td>
-                <td>{{ o.value | numberFormat 'pt-BR'}}</td>
-                <td class="minha-class" :class="{ 'pago': o.done, 'nao-pago': !o.done }">
-                    {{ o.done | doneLabel }}
-                </td>
-                <td>
-                    <a v-link="{ name: 'bill-pay.update', params: { id: o.id}}">Editar</a>
-                    <a href="#" @click.prevent="deleteBill(o)">Excluir</a>
-                </td>
-            </tr>
-        </tdody>
-    </table>
+    <div class="container">
+        <div class="row">
+            <table border="1" cellpadding="10">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Vencimento</th>
+                    <th>Nome</th>
+                    <th>Valor</th>
+                    <th>Paga?</th>
+                    <th>Ações</th>
+                </tr>
+                </thead>
+                <tdody>
+                    <tr v-for="(index,o) in bills">
+                        <td>{{ index + 1}}</td>
+                        <td>{{ o.date_due | dateFormat 'pt-BR'}}</td>
+                        <td>{{ o.name | stringFormat }}</td>
+                        <td>{{ o.value | numberFormat 'pt-BR'}}</td>
+                        <td class="minha-class" :class="{ 'pago': o.done, 'nao-pago': !o.done }">
+                            {{ o.done | doneLabel }}
+                        </td>
+                        <td>
+                            <a v-link="{ name: 'bill-pay.update', params: { id: o.id}}">Editar</a>
+                            <a href="#" @click.prevent="deleteBill(o)">Excluir</a>
+                        </td>
+                    </tr>
+                </tdody>
+            </table>
+        </div>
+    </div>
     `,
     data() {
         return {
--- src/bill-pay/bill-pay.component.js
+++ src/bill-pay/bill-pay.component.js
@@ -14,10 +14,18 @@ window.billPayComponent = Vue.extend({
             color: gray;
         }
     </style>
-    <h1>{{ title }}</h1>
-    <h3 :class="{'gray': status === false , 'green': status == 0, 'red': status > 0}">{{ status | statusGeneralPay}}</h3>
-    <h3>{{ total | numberFormat }}</h3>
-    <menu-component></menu-component>
+    <div class="section">
+        <div class="container">
+            <h1>{{ title }}</h1>
+            <h3 :class="{'gray': status === false , 'green': status == 0, 'red': status > 0}">{{ status | statusGeneralPay}}</h3>
+            <div class="row">
+                <div class="col s5 offset-s7">
+                    <h3>{{ total | numberFormat }}</h3>
+                </div>
+            </div>
+            <menu-component></menu-component>
+         </div>
+    </div>
     <router-view></router-view>
     `,
     data() {
